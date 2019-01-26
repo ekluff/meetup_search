5de79f8393c7e2c9d23bf6f237d5f816cceecb60
Evan Clough <evan@evanclough.com>
2019-01-26 23:21:04 +0000

    Tests and basic functionality for events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,28 @@
 class EventsController < ApplicationController
   def search
+    query = EventQueryWorker.perform_async(event_params)
+
+    if query
+      flash[:success] = 'Request is being processed and results will be available shortly.'
+      # using redirect instead of render so the user can refresh the page after searching without resubmitting the form
+      redirect_to action: 'index', text_query: event_params[:text_query]
+    else
+      flash[:error] = 'There was a problem processing your request. Please try again.'
+      head :internal_server_error
+    end
   end
 
   def index
+    if @events = Rails.cache.read(event_params[:text_query])
+      render @events
+    else
+      head :ok
+    end
   end
 
   private
 
   def event_params
-    #code
+    params.permit(:text_query)
   end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,18 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  let(:params) do
+    { 'text_query': 'skiing' }
+  end
 
-  describe "GET #search" do
-    it "returns http success" do
-      get :search
-      expect(response).to have_http_status(:success)
+  describe 'POST #search' do
+    let(:subject) { post :search, params: params }
+    context 'when job queues successfully' do
+      before { allow(EventQueryWorker).to receive(:perform_async).and_return(true) }
+
+      it 'calls the worker and renders the correct response' do
+        subject
+
+        expect(flash[:success]).to eq('Request is being processed and results will be available shortly.')
+        expect(response.status).to eq(302)
+        expect(EventQueryWorker).to have_received(:perform_async)
+      end
+    end
+
+    context 'when job fails to queue' do
+      before { allow(EventQueryWorker).to receive(:perform_async).and_return(nil) }
+
+      it 'calls the worker and renders the correct response' do
+        subject
+
+        expect(flash[:error]).to eq('There was a problem processing your request. Please try again.')
+        expect(response.status).to eq(500)
+      end
     end
   end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe 'GET #index' do
+    let(:subject) { get :index, params: params }
+
+    before { subject }
+
+    context 'before the results are available' do
+      it 'returns nothing' do
+        expect(response.body).to eq('')
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when the results are available' do
+      it 'displays a list of 10 events' do
+        # coming back to this when I know what the payload looks like
+      end
     end
   end
 
