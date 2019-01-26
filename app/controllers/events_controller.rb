b865958677db67338828343be55a5f391d237974
Evan Clough <evan@evanclough.com>
2019-01-26 23:39:02 +0000

    Rearrange events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,28 +1,29 @@
 class EventsController < ApplicationController
-  def search
-    query = EventQueryWorker.perform_async(event_params)
-
-    if query
-      flash[:success] = 'Request is being processed and results will be available shortly.'
-      # using redirect instead of render so the user can refresh the page after searching without resubmitting the form
-      redirect_to action: 'index', text_query: event_params[:text_query]
-    else
-      flash[:error] = 'There was a problem processing your request. Please try again.'
-      head :internal_server_error
-    end
-  end
-
   def index
-    if @events = Rails.cache.read(event_params[:text_query])
+    # this won't work for an empty query
+    if @events = Rails.cache.read(event_params[:query_string])
       render @events
     else
       head :ok
     end
   end
 
+  def search
+    query = EventQueryWorker.perform_async(event_params)
+
+    if query
+      flash[:success] = 'Request is being processed and results will be available shortly.'
+      # using redirect instead of render so the user can refresh the page after searching without resubmitting the form
+      redirect_to action: 'index', query_string: event_params[:query_string]
+    else
+      flash[:error] = 'There was a problem processing your request. Please try again.'
+      head :internal_server_error
+    end
+  end
+
   private
 
   def event_params
-    params.permit(:text_query)
+    params.permit(:query_string)
   end
 end
