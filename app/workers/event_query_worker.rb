597e6cadc97bd908ec27bff533c198e89133ee69
Evan Clough <evan@evanclough.com>
2019-01-27 03:34:10 +0000

    Event query worker and worker specs

--- app/workers/event_query_worker.rb
+++ app/workers/event_query_worker.rb
@@ -1,7 +1,46 @@
 class EventQueryWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  CITY = 'München'
+  COUNTRY = 'DE'
+  PAGE = 10
+  # Authenticating against the API with a personal key rather than a more complex oauth flow
+  # You'll need to write your own key into the encrypted credentials file. See README
+  KEY = Rails.application.credentials.meetup[:api_key]
+
+  def perform(query_string)
+    return if Rails.cache.read cache_key(query_string)
+    query_and_cache query_string
+  end
+
+  private
+
+  # this is necessary so that there is a key even if there is no input
+  def cache_key(query_string)
+    "_key_#{query_string}"
+  end
+
+  # This uses the v2 open_events endpoint on the Meetup API. This is the only endpoint
+  # of the API that allows searching of events directly by location. To use the V3 events
+  # endpoint, it would be necessary to first search groups by location and then retrieve
+  # events belonging to those groups. I didn't see any information about an
+  # include directive being available to sideload events with groups.
+  def query_and_cache(query_string)
+    uri = 'https://api.meetup.com/2/open_events'
+    params = params(query_string)
+    response = Faraday.get uri, params
+
+    if response.success?
+      Rails.cache.write cache_key(query_string), JSON.parse(response.body)
+    else
+      # In a real app we would want to put care into error handling
+      # Here the idea is to recognize this is a place that would need error handling
+      raise "Network error"
+    end
+  end
+
+  # removes 'text' if query_string is nil
+  def params(query_string)
+    { text: query_string, city: CITY, country: COUNTRY, key: KEY }.compact
   end
 end
--- spec/workers/event_query_worker_spec.rb
+++ spec/workers/event_query_worker_spec.rb
@@ -1,4 +1,42 @@
 require 'rails_helper'
+
 RSpec.describe EventQueryWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:worker) { EventQueryWorker.new }
+  let(:query_string) { 'skiing' }
+  let(:subject) { worker.perform query_string }
+  let(:meetup_response) { file_fixture('meetup_response.json').read }
+
+  let!(:stub) { stub_request(:get, /https:\/\/api\.meetup\.com\/2\/open_events/)
+    .with(query: hash_including({ text: query_string }))
+    .to_return(status: 200, body: meetup_response) }
+  let!(:stub2) { stub_request(:get, /https:\/\/api\.meetup\.com\/2\/open_events/)
+    .with(query: hash_excluding({ text: query_string }))
+    .to_return(status: 200, body: meetup_response) }
+
+  before do
+    Rails.cache.clear
+    subject
+  end
+
+  it 'calls the Meetup API and caches the results' do
+    expect(stub).to have_been_requested
+    expect(Rails.cache.read "_key_#{query_string}").to match(JSON.parse(meetup_response))
+  end
+
+  context 'when reusing a prior query' do
+    # It's tempting to use timecop to test the five minute window, but we shouldn't
+    # do that because we set Redis to expire the cache and we don't want to test the framework
+    it 'it does not call the API if value is already cached' do
+      subject
+      expect(stub).to have_been_requested.once
+    end
+  end
+
+  context 'when query_string is nil' do
+    let(:query_string) { nil }
+
+    it 'works' do
+      expect(stub2).to have_been_requested
+    end
+  end
 end
