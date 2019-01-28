0eca738fe7290124bee100786b36ad99c69bb9b0
Evan Clough <evan@evanclough.com>
2019-01-28 06:23:04 +0000

    Javascript to poll index endpoint

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,2 +1,26 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+let queryParams = new URLSearchParams(location.search);
+
+function triggerPoll() {
+  if ($('.event-tile').length===0) {
+    setTimeout(pollIndex, 5000);
+  } else {
+    $('#loading-message').empty();
+  }
+};
+
+function pollIndex() {
+  // not sure why this doesn't happen until the second time through this function
+  $('#loading-message').html('<p>loading...</p>');
+
+  if ($('.event-tile').length===0) {
+    $.ajax({
+      url: 'events/index',
+      data: queryParams.toString(),
+      type: 'GET',
+      dataType: 'script',
+      complete: triggerPoll,
+    });
+  }
+};
+
+$( pollIndex() );
