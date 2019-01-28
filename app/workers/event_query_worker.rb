92bae44fae1709a4baf4b8e782ac9988ff53e815
Evan Clough <evan@evanclough.com>
2019-01-28 06:31:10 +0000

    Change 'query_string' to 'search_string'

--- app/workers/event_query_worker.rb
+++ app/workers/event_query_worker.rb
@@ -8,16 +8,16 @@ class EventQueryWorker
   # You'll need to write your own key into the encrypted credentials file. See README
   KEY = Rails.application.credentials.meetup[:api_key]
 
-  def perform(query_string)
-    return if Rails.cache.read cache_key(query_string)
-    query_and_cache query_string
+  def perform(search_string)
+    return if Rails.cache.read cache_key(search_string)
+    query_and_cache search_string
   end
 
   private
 
   # this is necessary so that there is a key even if there is no input
-  def cache_key(query_string)
-    "_key_#{query_string}"
+  def cache_key(search_string)
+    "_key_#{search_string}"
   end
 
   # This uses the v2 open_events endpoint on the Meetup API. This is the only endpoint
@@ -25,13 +25,18 @@ class EventQueryWorker
   # endpoint, it would be necessary to first search groups by location and then retrieve
   # events belonging to those groups. I didn't see any information about an
   # include directive being available to sideload events with groups.
-  def query_and_cache(query_string)
+  #
+  # A known issue here is that the instructions for this challenge mention viewing past events.
+  # The docs say it is possible to view both upcoming and past events with the
+  # value 'status=past,upcoming', however, this is incorrect. The endpoint appears to
+  # require only one value or the other, despite what the docs say.
+  def query_and_cache(search_string)
     uri = 'https://api.meetup.com/2/open_events'
-    params = params(query_string)
+    params = params(search_string)
     response = Faraday.get uri, params
 
     if response.success?
-      Rails.cache.write cache_key(query_string), JSON.parse(response.body)
+      Rails.cache.write cache_key(search_string), JSON.parse(response.body)
     else
       # In a real app we would want to put care into error handling
       # Here the idea is to recognize this is a place that would need error handling
@@ -39,8 +44,8 @@ class EventQueryWorker
     end
   end
 
-  # removes 'text' if query_string is nil
-  def params(query_string)
-    { text: query_string, city: CITY, country: COUNTRY, key: KEY }.compact
+  # removes 'text' if search_string is nil
+  def params(search_string)
+    { text: search_string, city: CITY, country: COUNTRY, key: KEY }.compact
   end
 end
