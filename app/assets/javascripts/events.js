2f7367f38b2cdd09dfc6dfe706fb24ed77ab74f0
Evan Clough <evan@evanclough.com>
2019-01-28 08:29:06 +0000

    Add comment

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,7 @@
+// This polling could possibly be simpler using some of the newer Rails features such as ActionCable.
+// I chose to write this from scratch because 1) it makes the app lighter and 2) I haven't written
+// something like this in a while and it was good practice.
+
 let queryParams = new URLSearchParams(location.search);
 
 function triggerPoll() {
